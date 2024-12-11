0a2e8ca89bf1c8681ab6ff4510fe89b498db6256
Carolina Cardoso (Jay) <carolinaliboriocardoso@gmail.com>
2024-12-11 15:08:59 +0000

    added theRaven_words.txt inside the text directory and a way of using said file. This file is incomplete.

---
+++ js/words.js
@@ -0,0 +1,96 @@
+const fs = require('fs');
+const path = require('path');
+
+//path para o ficheiro de texto
+const filePath = path.join(__dirname, '../assets/poem_file', 'theRaven_words.txt');
+
+let words = [];
+
+function loadWords() {
+    try {
+        const data = fs.readFileSync(filePath, 'utf8'); 
+        words = data.split("+"); // dividir usando +
+    } catch (err) {
+        console.error("Error reading the file:", err.message);
+    }
+}
+
+function* wordGenerator() {
+    for (const word of words) {
+        yield word;
+    }
+}
+
+loadWords();
+const generator = wordGenerator();
+
+function getNextWord() {
+    const next = generator.next();
+    if (!next.done) {
+        return next.value;
+    }
+}
+
+console.log(getNextWord());
+console.log(getNextWord());
+console.log(getNextWord());
+console.log(getNextWord());
+console.log(getNextWord());
+console.log(getNextWord());
+console.log(getNextWord());
+console.log(getNextWord());
+console.log(getNextWord());
+console.log(getNextWord());
+console.log(getNextWord());
+console.log(getNextWord());
+console.log(getNextWord());
+console.log(getNextWord());
+console.log(getNextWord());
+console.log(getNextWord());
+console.log(getNextWord());
+console.log(getNextWord());
+console.log(getNextWord());
+console.log(getNextWord());
+console.log(getNextWord());
+console.log(getNextWord());
+console.log(getNextWord());
+console.log(getNextWord());
+console.log(getNextWord());
+console.log(getNextWord());
+console.log(getNextWord());
+console.log(getNextWord());
+console.log(getNextWord());
+console.log(getNextWord());
+console.log(getNextWord());
+console.log(getNextWord());
+console.log(getNextWord());
+console.log(getNextWord());
+console.log(getNextWord());
+console.log(getNextWord());
+console.log(getNextWord());
+console.log(getNextWord());
+console.log(getNextWord());
+console.log(getNextWord());
+console.log(getNextWord());
+console.log(getNextWord());
+console.log(getNextWord());
+console.log(getNextWord());
+console.log(getNextWord());
+console.log(getNextWord());
+console.log(getNextWord());
+console.log(getNextWord());
+console.log(getNextWord());
+console.log(getNextWord());
+console.log(getNextWord());
+console.log(getNextWord());
+console.log(getNextWord());
+console.log(getNextWord());
+console.log(getNextWord());
+console.log(getNextWord());
+console.log(getNextWord());
+console.log(getNextWord());
+console.log(getNextWord());
+console.log(getNextWord());
+console.log(getNextWord());
+console.log(getNextWord());
+console.log(getNextWord());
